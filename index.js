7c256b623628eb7fc09d5b3c8d985a51f83dbd69
Croupierr <47375354+Croupierr@users.noreply.github.com>
2019-03-04 21:50:54 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const api = require('./api');
+const app = express();
+const port = 3000;
+
+app.all('/', (req, res)=>{
+    console.log('http://' + req.hostname + req.path);
+    res.send
+})
